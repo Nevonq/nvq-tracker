82f009f764c26f7dc4e2eeea4a6c52f9547cdc7f
n3vonq <107314027+Nevonq@users.noreply.github.com>
2024-07-14 21:52:53 +0300

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -7,8 +7,7 @@ AddEventHandler('nvq-tracker:setMarker', function()
     local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)
 
     if DoesEntityExist(vehicle) then
-
-		local animDict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@"
+        local animDict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@"
         local animName = "machinic_loop_mechandplayer"
 
         RequestAnimDict(animDict)
@@ -16,61 +15,52 @@ AddEventHandler('nvq-tracker:setMarker', function()
             Citizen.Wait(100)
         end
 
-        -- Play the animation
         TaskPlayAnim(playerPed, animDict, animName, 8.0, -8.0, -1, 49, 0, false, false, false)
-        -- Wait for the animation to complete (adjust timing as necessary)
-        if Config.UseProgress then
-            exports.rprogress:Start(_U('rprogress'), 5000)
 
+        if Config.UseProgress then
+            exports.rprogress:Start(TranslateCap('rprogress'), 5000)
+        else 
+            Citizen.Wait(5000)
         end
 
-        -- Citizen.Wait(5000)
-
-        -- Stop the animation
         StopAnimTask(playerPed, animDict, animName, 1.0)
 
         local plate = GetVehicleNumberPlateText(vehicle)
         local vehicleCoords = GetEntityCoords(vehicle)
-        local blip =  AddBlipForCoord(vehicleCoords.x, vehicleCoords.y, vehicleCoords.z)
-        SetBlipSprite(blip, Config.BlipSprite)  -- Change the blip icon to a car
-        SetBlipColour(blip, Config.BlipColour)    -- Change the blip color, e.g., blue
-        SetBlipScale(blip, Config.BlipScale)   -- Set the blip scale
+
+        local blip = AddBlipForCoord(vehicleCoords.x, vehicleCoords.y, vehicleCoords.z)
+        SetBlipSprite(blip, Config.BlipSprite)
+        SetBlipColour(blip, Config.BlipColour)
+        SetBlipScale(blip, Config.BlipScale)
         SetBlipAsShortRange(blip, true)
 
         BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString('<font face=\'Roboto\'>'.._U('veh')..plate)
+        AddTextComponentString('<font face=\'Roboto\'>'..TranslateCap('veh')..plate)
         EndTextCommandSetBlipName(blip)
-        ESX.ShowNotification(_U('suc')..plate)
- 
 
-           -- Monitor the vehicle
-           Citizen.CreateThread(function()
+        ESX.ShowNotification(TranslateCap('suc')..plate)
+
+        Citizen.CreateThread(function()
             local lastCoords = vehicleCoords
             while DoesEntityExist(vehicle) do
                 lastCoords = GetEntityCoords(vehicle)
+                SetBlipCoords(blip, lastCoords.x, lastCoords.y, lastCoords.z)
                 Citizen.Wait(1000)
             end
 
-            -- Vehicle deleted, update the blip
-        -- local lastCoords = GetEntityCoords(vehicle)
             SetBlipCoords(blip, lastCoords.x, lastCoords.y, lastCoords.z)
             SetBlipColour(blip, 1)  
             SetBlipAlpha(blip, 255)
 
-            ESX.ShowNotification(_U('err'))
+            ESX.ShowNotification(TranslateCap('err'))
 
-            Citizen.Wait(Config.DeletedTime)  -- Wait
+            Citizen.Wait(Config.DeletedTime) 
 
-            -- Delete the blip
             RemoveBlip(blip)
         end)
 
-
-
-		TriggerServerEvent('nvq-tracker:removeGPS')
-
+        TriggerServerEvent('nvq-tracker:removeGPS')
     else
-        ESX.ShowNotification(_U('no_veh'))
+        ESX.ShowNotification(TranslateCap('no_veh'))
     end
 end)
-
